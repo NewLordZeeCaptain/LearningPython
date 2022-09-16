6e9c93b648c24463f13ab94cb1ecc5bdbd5aceb9
newlordzeecaptain <lordzeecaptain@ya.ru>
2022-09-16 20:51:03 +0300

    I dont know what is this

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
-for i in range(-10,10):
-    print(i)
+import shutil
+file = "new.py"
+file_copy = "new_copy.py"
+try:
+    shutil.copyfile(file,file_copy)
+    shutil.copy2(file, file_copy)
+finally:
+    print("End")
