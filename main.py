1e7ab157ee419b9be6ae3deb21dd7be588ce3a6e
newlordzeecaptain <lordzeecaptain@ya.ru>
2022-09-29 17:56:36 +0300

    1

--- main.py
+++ main.py
@@ -4,22 +4,3 @@ print(testingFile.read())
 addedTest = input("If you would like to write something in file, paste it here, if not just paste NO: ")
 testingFile.write(addedTest)
 print(testingFile.read())
-
-# 
-[SwitchyOmega Conditions]
-@with result
-
-*.yandex.* +direct
-*.vk.* +direct
-*.animego.* +direct
-*.aniboom.* +direct
-*.mangalib.* +direct
-*.dzen.ru +direct
-*.kinopoisk.ru +direct
-192.168.31.* +direct
-*.pikabu.ru +direct
-*.gosuslugi.ru +direct
-*.netology.* +direct
-*.dvfu.ru +direct
-
-* +proxy
