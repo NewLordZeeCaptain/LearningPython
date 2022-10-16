c0c046c3797dcd15d9de0954b8edb92ae0987163
newlordzeecaptain <lordzeecaptain@ya.ru>
2022-10-16 12:19:19 +0300

    testing

--- main.py
+++ main.py
@@ -1,67 +1,69 @@
-documents = [
-    {"type": "passport", "number": "2207 876234", "name": "Василий Гупкин"},
-    {"type": "invoice", "number": "11-2", "name": "Геннадий Покемонов"},
-    {"type": "insurance", "number": "10006", "name": "Аристарх Павлов"},
-]
-directories = {"1": ["2207 876234", "11-2", "5455 028765"], "2": ["10006"], "3": []}
+import telebot
+import sqlite3
 
+db = sqlite3.connect("Server.db", check_same_thread=False)
+sql = db.cursor()
 
-def people():
-    number = input("Введите номер документа: ")
-    for document in documents:
-        if number in document["number"]:
-            print(document["name"])
-            return
-    print("Данного человека нет")
+sql.execute(
+    """
+    CREATE TABLE IF NOT EXISTS telegram_users (
+    user_ID INT,
+    user_firstname TEXT,
+    user_lastname TEXT
 
-
-def search():
-    number = input("Введите номер документа: ")
-    for key, value in directories.items():
-        if number in value:
-            print(key)
-            return
-
-    print("Такого документа нет")
-
-
-def add():
-    number, type_of, name, place = (
-        input("Укажите номер: "),
-        input("Тип документа: "),
-        input("Имя: "),
-        input("Полку: "),
     )
-    if place not in directories:
-        print("Такой полки нет")
-        return
-
-    directories[place].append(number)
-    documents.append({"type": type_of, "number": number, "name": name})
+    """
+)
+db.commit()
+bot = telebot.TeleBot("5588570551:AAHWgQOIoHj2KIaBsRYbzsH4IOPc_VOGhwA")
 
 
-def document_list():
-    pass
+persons = dict()
 
 
-def main():
-    while True:
-        command = input()
-        if command == "p":
-            people()
+class User:
+    def __init__(self, user_id, first_name, last_name) -> None:
+        self.user_id = user_id
+        self.first_name = first_name
+        self.last_name = last_name
 
-        elif command == "s":
-            search()
+    def getData(self):
+        return f"FirstName: {self.first_name}\nLastName: {self.last_name}"
 
-        elif command == "l":
-            document_list()
+    def addtoDB(self):
+        if sql.fetchone() is None:
+            sql.execute(
+                "INSERT INTO telegram_users VALUES (?,?,?)",
+                (self.user_id, self.first_name, self.last_name),
+            )
+            db.commit()
+        else:
+            pass
 
-        elif command == "a":
-            add()
 
-        elif command == "q":
-            print("Goodbye")
-            break
+@bot.message_handler(commands=["start"])
+def start(message):
+    newUser = User(
+        message.from_user.id, message.from_user.first_name, message.from_user.last_name
+    )
+    newUser.addtoDB()
+
+    mess = f"Hello, <b>{message.from_user.first_name} <u>{message.from_user.last_name}</u></b>"
+    bot.send_message(message.chat.id, mess, parse_mode="html")
 
 
-main()
+# @bot.message_handler()
+# def get_user_texxt(message):
+#     if message.text == "hello":
+#         bot.send_message(message.chat.id, "Hi, bro!", parse_mode="html")
+#     elif message.text == "id":
+#         bot.send_message(message.chat.id, f"Your id: {message.from_user.id}")
+
+
+@bot.message_handler(commands=["person"])
+def get_stored_data(message):
+    for value in sql.execute("SELECT * FROM telegram_users"):
+        bot.send_message(message.chat.id, value[2])
+
+
+bot.polling(non_stop=True)
